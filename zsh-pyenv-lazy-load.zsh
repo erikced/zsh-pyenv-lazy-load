5cab56cde752fbacf9af7dd175852ef2cab1e207
Erik Cederberg <erik.cederberg@amra.se>
2018-01-10 14:22:19 +0100

    Add lazy-load plugin for pyenv
    Registers a chpwd hook which is executed each time the directory is
    changed and adds lazy-loading of the pyenv command.

---
+++ zsh-pyenv-lazy-load.zsh
@@ -0,0 +1,32 @@
+if type pyenv &> /dev/null; then
+  return
+fi
+
+_init_pyenv() {
+  unset -f pyenv _pyenv_chpwd_hook _init_pyenv
+  chpwd_functions[$chpwd_functions[(i)_pyenv_chpwd_hook]]=()
+
+  if [[ ! "$PATH" == */.pyenv/bin* ]]; then
+    export PATH="$HOME/.pyenv/bin:$PATH"
+  fi
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
+}
+
+pyenv() {
+  _init_pyenv
+  pyenv "$@"
+}
+
+_pyenv_chpwd_hook() {
+  local DIR=$PWD
+  while [ "$DIR" != "/" ]; do
+    if [ -f "$DIR/.python-version" ]; then
+      _init_pyenv
+      break
+    fi
+    DIR=$DIR:h
+  done
+}
+
+export chpwd_functions=($chpwd_functions _pyenv_chpwd_hook)
